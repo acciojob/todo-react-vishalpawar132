5b56eefc7c3e8446ea5e3350d1db46258a40fbc9
vishal pawar <pawarvishal29999@gmail.com>
2023-09-20 10:47:19 +0530

    submit1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 
 import React from "react";
 import './../styles/App.css';
+import TodoList from "./TodoList";
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+const App = () =>{
+    return (
+      <div className="App">
+        <TodoList />
+      </div>
+    );
+  }
+
 
 export default App
---
+++ src/components/TodoList.js
@@ -0,0 +1,48 @@
+
+
+import React, { useState } from 'react';
+
+const TodoList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const handleAddTask = () => {
+    if (newTask.trim() === '') {
+      alert('Please enter a task.');
+      return;
+    }
+
+    setTasks([...tasks, newTask]);
+    setNewTask('');
+  };
+
+  const handleRemoveTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div className="todo-list">
+      <h1>To-Do List</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Enter a new task"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button onClick={handleAddTask}>Add Todo</button>
+      </div>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button onClick={() => handleRemoveTask(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
